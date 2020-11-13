a88e89c85e1aeb5ddcae2c0d7818b99d39149a3d
João Prates <jvdprates@hotmail.com>
2020-11-13 15:27:52 -0300

    Fix: Corrigindo validação de email

--- src/validators/sessionValidator.js
+++ src/validators/sessionValidator.js
@@ -3,14 +3,14 @@ const { Segments, Joi } = require("celebrate");
 const sessionValidator = {
   login: {
     [Segments.BODY]: Joi.object().keys({
-      email: Joi.string().email().required(),
+      email: Joi.string().required(),
       password: Joi.string().required(),
     }),
   },
 
   forgotPassword: {
     [Segments.BODY]: Joi.object().keys({
-      email: Joi.string().email().required(),
+      email: Joi.string().required(),
     }),
   },
 };
